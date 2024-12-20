d49f82c481e579cc22699765df069ba51295be43
romy <romyjimmy17@gmail.com>
2024-12-20 08:52:48 +0530

    remove files

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,40 +1,13 @@
 import logo from "../assets/images/logo.png";
 
-// icons images
-import google from "../assets/icons/google.svg";
-import twitter from "../assets/icons/twitter.svg";
-import google_play from "../assets/images/google_play.jpg";
-import app_store from "../assets/images/app_store.jpg";
-import heart from "../assets/icons/heart.svg";
-import user from "../assets/icons/user.svg";
-import cart from "../assets/icons/cart.svg";
 
-
-
-// colors images
-import color1 from "../assets/images/color1.svg";
-import color2 from "../assets/images/color2.svg";
-import color3 from "../assets/images/color3.svg";
-import color4 from "../assets/images/color4.svg";
-import color5 from "../assets/images/color5.svg";
-import color6 from "../assets/images/color6.svg";
-import color7 from "../assets/images/color7.svg";
-import color8 from "../assets/images/color8.svg";
-import color9 from "../assets/images/color9.svg";
-import color10 from "../assets/images/color10.svg";
-import color11 from "../assets/images/color11.svg";
-import color12 from "../assets/images/color1.svg";
 
 
 // product services images
-import card_icon from "../assets/icons/card_icon.svg";
-import size_icon from "../assets/icons/size_icon.svg";
-import shipping_icon from "../assets/icons/shipping_icon.svg";
-import return_icon from "../assets/icons/return_icon.svg";
+
 
 // cart images
 
-import play_icon from "../assets/icons/play_icon.svg";
 
 // error
 import page_not_found from "../assets/images/page_not_found.png";
@@ -44,45 +17,12 @@ import empty_cart_img from "../assets/images/empty_cart_img.svg";
 // wishlist
 import green_heart from "../assets/icons/green_heart.svg";
 
-// card
-import googlePay from "../assets/images/googlePay.png";
-import visa from "../assets/images/visa.png";
-import paypal from "../assets/images/paypal.png";
-import paypass from "../assets/images/paypass.png";
-import confirmed_img from "../assets/images/confirmed_img.svg";
+
 
 export const staticImages = {
   logo,
-  google,
-  twitter,
-  google_play,
-  app_store,
-  heart,
-  user,
-  cart,
-  color1,
-  color2,
-  color3,
-  color4,
-  color5,
-  color6,
-  color7,
-  color8,
-  color9,
-  color10,
-  color11,
-  color12,
-  card_icon,
-  size_icon,
-  shipping_icon,
-  return_icon,
-  play_icon,
+ 
+  green_heart,
   page_not_found,
   empty_cart_img,
-  green_heart,
-  googlePay,
-  visa,
-  paypal,
-  paypass,
-  confirmed_img,
 };
